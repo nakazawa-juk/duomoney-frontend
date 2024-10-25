ec3cf3cd03b76833aac67ef458ce7518ee86b569
Nakazawa-Jukiya <nakazawamusc@gmail.com>
2024-10-25 17:40:10 +0900

    refactor: 不要なコードの削除

--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -103,16 +103,9 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Vue } from 'vue-class-component';
 
-@Options({
-  props: {
-    msg: String,
-  },
-})
-export default class HelloWorld extends Vue {
-  msg!: string;
-}
+export default class HelloWorld extends Vue {}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
