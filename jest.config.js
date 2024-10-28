0a10bf04ba20dfa341dd6e9d5ea888f16ae41c3f
Nakazawa-Jukiya <nakazawamusc@gmail.com>
2024-10-28 16:28:33 +0900

    fix: jest test coverageを生成しないようにする。画面の重要なテストはe2eで行い、unit testはそこまで重要ではなくcoverageの必要性が低い。

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript',
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.{js,vue,ts}'],
-  coverageReporters: ['html', 'text-summary'],
-  coveragePathIgnorePatterns: [
-    'src/main.ts', // ここでmain.tsを除外
-  ],
+
+  // テストのカバレッジが欲しければ以下のコメントアウトを外す
+  // collectCoverage: true,
+  // collectCoverageFrom: ['<rootDir>/src/**/*.{js,vue,ts}'],
+  // coverageReporters: ['html', 'text-summary'],
+  // coveragePathIgnorePatterns: [
+  //   'src/main.ts', // ここでmain.tsを除外
+  // ],
 };
